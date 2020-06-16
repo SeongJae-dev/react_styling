b6ced405998e6ecc1587e430eca8b28aeae8bf67
chiron8963@gmail.com <chiron1217@naver.com>
2020-06-16 23:05:01 +0900

    StyledComponent

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import SassComponent from "./component/SassComponent";
 import CSSModule from "./component/CSSModule";
+import StyledComponent from "./component/StyledComponent";
 
 function App() {
   return (
     <div>
       {/*<SassComponent />*/}
-      <CSSModule/>
+      {/*<CSSModule/>*/}
+      <StyledComponent/>
     </div>
   );
 }
--- src/component/CSSModule.js
+++ src/component/CSSModule.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import style from '../CSSModule.module.css'
+import classNames from 'classnames/bind';
+import style from '../scss/CSSModule.module.scss'
+
+const cx = classNames.bind(style);
 
 const CSSModule = () => {
     return (
-        <div className={`${style.wrapper} ${style.inverted}`}>
+        <div className={cx('wrapper','inverted')}>
             hello <span className="something"> CSS Module</span>
         </div>
     );
